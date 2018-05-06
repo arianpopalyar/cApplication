ca92eb6f6aec0297625dd2c1f66a9eb49e83ee9a
arian popalyar <arian_popalyar@yahoo.com>
2018-05-06 20:07:00 +0100

    my changes

---
+++ Quirky/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Quirky.MvcApplication" Language="C#" %>
---
+++ Quirky/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quirky.Startup))]
+namespace Quirky
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
